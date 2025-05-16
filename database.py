2eea5761231aa81d0114d4ff335d4058a930ff7e
FABIO L SANTOS <78166985+fabiolucasz@users.noreply.github.com>
2025-05-16 19:51:48 -0300

    Update database.py

--- database.py
+++ database.py
@@ -19,37 +19,66 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-
-novo_aluno = Aluno(nome="Riquelme joga fácil", idade=55)
-session.add(novo_aluno)
-session.commit()
-
-
-
-
-# Buscar aluno
-aluno = session.query(Aluno).filter(Aluno.nome == "Ybson").first()
-
-if aluno:
-    aluno.idade = 15
+def criar_aluno():
+    novo_aluno = Aluno(nome="Riquelme joga fácil", idade=55)
+    session.add(novo_aluno)
     session.commit()
-    print("Aluno atualizado com sucesso!")
-else:
-    print("Aluno não encontrado.")
-
-# Buscar aluno
-aluno = session.query(Aluno).filter(Aluno.id == 4).first()
-
-if aluno:
-    session.delete(aluno)
-    session.commit()
-    print("Aluno removido com sucesso!")
-else:
-    print("Aluno não encontrado.")
 
 
+def editar_aluno():
+    aluno = session.query(Aluno).filter(Aluno.nome == "Ybson").first()
 
-alunos = session.query(Aluno).all()
-for aluno in alunos:
-    print(f"ID: {aluno.id} | Nome: {aluno.nome} | Idade: {aluno.idade}")
+    if aluno:
+        aluno.idade = 15
+        session.commit()
+        print("Aluno atualizado com sucesso!")
+    else:
+        print("Aluno não encontrado.")
 
+def deletar_aluno():
+    aluno = session.query(Aluno).filter(Aluno.id == 4).first()
+
+    if aluno:
+        session.delete(aluno)
+        session.commit()
+        print("Aluno removido com sucesso!")
+    else:
+        print("Aluno não encontrado.")
+
+
+def listar_alunos():
+    lista.delete(0, tk.END)
+    alunos = session.query(Aluno).all()
+    for aluno in alunos:
+        lista.insert(tk.END, f"ID: {aluno.id} | Nome: {aluno.nome} | Idade: {aluno.idade}")
+
+# Criação da tela
+
+import tkinter as tk
+
+janela = tk.Tk()
+janela.geometry("400x500")
+
+#Campo nome
+label_nome = tk.Label(janela, text="Nome")
+label_nome.pack()
+entry_nome = tk.Entry(janela)
+entry_nome.pack()
+
+#Campo idade
+label_idade = tk.Label(janela, text="Idade")
+label_idade.pack()
+entry_idade = tk.Entry(janela)
+entry_idade.pack()
+
+#Botão adicionar aluno
+btn_adicionar_aluno = tk.Button(janela, text="Adicionar",
+                                command=criar_aluno)
+btn_adicionar_aluno.pack()
+
+lista = tk.Listbox(janela, width=50, height=15)
+lista.pack()
+
+
+listar_alunos()
+janela.mainloop()
